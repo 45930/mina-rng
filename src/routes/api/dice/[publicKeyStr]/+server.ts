6b5036ec0aee3a3c325d78fef1ba53237061da4b
Coby <contact@45930.xyz>
2023-01-02 16:58:12 -0500

    checking in

---
+++ src/routes/api/dice/[publicKeyStr]/+server.ts
@@ -0,0 +1,39 @@
+import { error } from '@sveltejs/kit';
+import type { RequestEvent } from '@sveltejs/kit';
+
+import { PrivateKey, Field, Encryption, Signature, PublicKey } from 'snarkyjs';
+import { isSnarkyLoaded, loadSnarky, oraclePrivateKeyStr } from '$lib/server/utils';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET(request: RequestEvent) {
+  if (!isSnarkyLoaded) {
+    await loadSnarky();
+  }
+
+  const publicKey = PublicKey.fromBase58(request.params.publicKeyStr!);
+  if (!publicKey) {
+    throw error(400, "Invalid Params, Public Key not valid");
+  }
+
+  const min = Math.floor(Number(request.url.searchParams.get('min'))) || 0;
+  const max = Math.floor(Number(request.url.searchParams.get('max'))) || 999999;
+  if (max <= min) {
+    throw error(400, "Invalid Params, max must be greater than min");
+  }
+
+  const range = max - min + 1;
+  const rand = Math.floor(Math.random() * range + min);
+  const oraclePrivateKey = PrivateKey.fromBase58(oraclePrivateKeyStr);
+  const encryption = Encryption.encrypt([Field(rand)], PublicKey.fromBase58(publicKey));
+  const sig = Signature.create(oraclePrivateKey, [
+    Field(min),
+    Field(max),
+    ...encryption.cipherText
+  ]);
+
+  return new Response(JSON.stringify({
+    publicKey: encryption.publicKey.toJSON(),
+    cipherText: encryption.cipherText.toString(),
+    signature: sig.toJSON()
+  }));
+}
--- tests/api/randomNumber.spec.ts
+++ tests/api/randomNumber.spec.ts
@@ -4,6 +4,21 @@ import { oraclePrivateKeyStr } from '../../src/lib/server/utils.js';
 
 await isReady;
 
+const parseRequest = (async (url: string, min: number, max: number) => {
+  const resp = await context.fetch(url)
+  const oracleData: ApiResponse = await resp.json();
+  const sig: Signature = Signature.fromJSON(oracleData.signature);
+  const ct: Field[] = oracleData.cipherText.split(',').map(f => Field(f));
+  const signatureInput: Field[] = [Field(min), Field(max), ...ct];
+  const group: Group = Group.fromJSON(oracleData.publicKey);
+  const plaintext = Encryption.decrypt({ publicKey: group, cipherText: ct }, executorPrivateKey).toString();
+  return {
+    sig,
+    signatureInput,
+    plaintext
+  }
+});
+
 // B62qm1fco5NmfzFu7b6YC9snnhTNLemCXfJJCM5mdeXEWW3seTtzWfY 
 // EKFM7iJ9rRb7Pz6txZEiGVvnw1yQPuhpBM1H5CtNqWVNXKS4Lbcy
 let executorPrivateKey: PrivateKey;
@@ -23,13 +38,7 @@ test.describe("Random Number", () => {
   test("Default Params", async () => {
     const min = 0;
     const max = 999_999;
-    const resp = await context.fetch(`http://127.0.0.1:5173/api/randomNumber/${executorPublicKey.toBase58()}`)
-    const oracleData: ApiResponse = await resp.json();
-    const sig: Signature = Signature.fromJSON(oracleData.signature);
-    const ct: Field[] = oracleData.cipherText.split(',').map(f => Field(f));
-    const signatureInput: Field[] = [Field(min), Field(max), ...ct];
-    const group: Group = Group.fromJSON(oracleData.publicKey);
-    const plaintext = Encryption.decrypt({ publicKey: group, cipherText: ct }, executorPrivateKey).toString();
+    const { sig, signatureInput, plaintext } = await parseRequest(`http://127.0.0.1:5173/api/randomNumber/${executorPublicKey.toBase58()}`, min, max);
     expect(sig.verify(oraclePublicKey, signatureInput).toBoolean()).toBeTruthy();
     expect(Number(plaintext)).toBeGreaterThanOrEqual(min);
     expect(Number(plaintext)).toBeLessThanOrEqual(max);
@@ -38,13 +47,7 @@ test.describe("Random Number", () => {
   test("Custom Params", async () => {
     const min = 5;
     const max = 7;
-    const resp = await context.fetch(`http://127.0.0.1:5173/api/randomNumber/${executorPublicKey.toBase58()}?min=${min}&max=${max}`)
-    const oracleData: ApiResponse = await resp.json();
-    const sig: Signature = Signature.fromJSON(oracleData.signature);
-    const ct: Field[] = oracleData.cipherText.split(',').map(f => Field(f));
-    const signatureInput: Field[] = [Field(min), Field(max), ...ct];
-    const group: Group = Group.fromJSON(oracleData.publicKey);
-    const plaintext = Encryption.decrypt({ publicKey: group, cipherText: ct }, executorPrivateKey).toString();
+    const { sig, signatureInput, plaintext } = await parseRequest(`http://127.0.0.1:5173/api/randomNumber/${executorPublicKey.toBase58()}`, min, max);
     expect(sig.verify(oraclePublicKey, signatureInput).toBoolean()).toBeTruthy();
     expect(Number(plaintext)).toBeGreaterThanOrEqual(min);
     expect(Number(plaintext)).toBeLessThanOrEqual(max);
