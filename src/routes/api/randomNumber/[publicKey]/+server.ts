a2a7a36850773a093f4150d5c219482b7ca490cd
Coby <contact@45930.xyz>
2023-01-02 19:30:31 -0500

    removing nr

--- src/routes/api/randomNumber/[publicKey]/+server.ts
+++ src/routes/api/randomNumber/[publicKey]/+server.ts
@@ -4,44 +4,32 @@ import type { RequestEvent } from '@sveltejs/kit';
 import { PrivateKey, Field, Encryption, Signature, PublicKey } from 'snarkyjs';
 import { isSnarkyLoaded, loadSnarky, oraclePrivateKeyStr } from '$lib/server/utils';
 
-import newrelic from 'newrelic';
-
 /** @type {import('./$types').RequestHandler} */
 export async function GET(request: RequestEvent) {
-  const response = await newrelic.startWebTransaction(request.url.toString(), async () => {
-    if (!isSnarkyLoaded) {
-      await loadSnarky();
-    }
+  if (!isSnarkyLoaded) {
+    await loadSnarky();
+  }
 
-    const publicKey = request.params.publicKey || '';
-    const min = Math.floor(Number(request.url.searchParams.get('min'))) || 0;
-    const max = Math.floor(Number(request.url.searchParams.get('max'))) || 999999;
-    if (max <= min) {
-      throw error(400, "Invalid Params, max must be greater than min");
-    }
-    newrelic.recordCustomEvent(
-      'RANDOM_NUMBER_API',
-      {
-        min,
-        max
-      }
-    );
-    const range = max - min + 1;
-    const rand = Math.floor(Math.random() * range + min);
-    const oraclePrivateKey = PrivateKey.fromBase58(oraclePrivateKeyStr);
-    const encryption = Encryption.encrypt([Field(rand)], PublicKey.fromBase58(publicKey));
-    const sig = Signature.create(oraclePrivateKey, [
-      Field(min),
-      Field(max),
-      ...encryption.cipherText
-    ]);
+  const publicKey = request.params.publicKey || '';
+  const min = Math.floor(Number(request.url.searchParams.get('min'))) || 0;
+  const max = Math.floor(Number(request.url.searchParams.get('max'))) || 999999;
+  if (max <= min) {
+    throw error(400, "Invalid Params, max must be greater than min");
+  }
+  const range = max - min + 1;
+  const rand = Math.floor(Math.random() * range + min);
+  console.info(`API RANDOM NUMBER: min=${min} max=${max} rand=${rand}`)
+  const oraclePrivateKey = PrivateKey.fromBase58(oraclePrivateKeyStr);
+  const encryption = Encryption.encrypt([Field(rand)], PublicKey.fromBase58(publicKey));
+  const sig = Signature.create(oraclePrivateKey, [
+    Field(min),
+    Field(max),
+    ...encryption.cipherText
+  ]);
 
-    return new Response(JSON.stringify({
-      publicKey: encryption.publicKey.toJSON(),
-      cipherText: encryption.cipherText.toString(),
-      signature: sig.toJSON()
-    }));
-  });
-
-  return response;
+  return new Response(JSON.stringify({
+    publicKey: encryption.publicKey.toJSON(),
+    cipherText: encryption.cipherText.toString(),
+    signature: sig.toJSON()
+  }));
 }
